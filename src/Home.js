359afce510c5fa2dd81d6799fc897843d0898f2e
mailjprachi <mail.jprachi@gmail.com>
2026-02-25 11:04:07 +0530

    logo change

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -65,7 +39,7 @@ const Home = () => {
     <img 
       src="logo.png" /* Replace 'logo.png' with the actual path to your uploaded logo file */
       alt="ResDe.ai Logo" 
-      className={`h-12 w-auto object-contain ${className}`} 
+      className={`h-20 w-auto object-contain ${className}`} 
     />
   );
 
